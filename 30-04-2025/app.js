2cf3145825db796bc7833fd34f21f343d739508b
MartinGabrielRamirezHilario <martin.ramirezhilario@gmail.com>
2025-05-03 13:56:12 -0600

    Modifique el codigo para levantar 3 servidores con express

--- 30-04-2025/app.js
+++ 30-04-2025/app.js
@@ -1,15 +1,28 @@
 //importar express
 const express=require('express');
 
-//creacion de una instancia
-const app=express();
+//creacion de una instancias
+const app1=express();
+const app2=express();
+const app3=express();
 
-//definir el puerto
-const port=5009;
+//definimos los puertos
+const port1=5009;
+const port2=5010;
+const port3=5011;
 
 //Configuracion de Ruta get para la raiz('/)
-app.get('/',(req,res)=>{
-    res.send('Hola Mundo');
+app1.get('/',(req,res)=>{
+    res.send('Hola Mundo desde el puerto 5009, Servidor 1');
 });
+app2.get('/',(req,res)=>{
+    res.send('Hola Mundo desde el puerto 5010, Servidor 2');
+});
+app3.get('/',(req,res)=>{
+    res.send('Hola Mundo desde el Puerto 5011, Servidor 3');
+})
 
-app.listen(port);
+
+app1.listen(port1);
+app2.listen(port2);
+app3.listen(port3);
